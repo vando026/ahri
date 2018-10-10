27880147403dcaf4f359580afca43505b740c4eb
Alain <vando026@umn.edu>
2018-10-10 08:30:08 +0200

    update getCVLData

--- R/getCVLData.R
+++ R/getCVLData.R
@@ -33,16 +33,13 @@ getCVLData <- function(Args) {
   dat <- filter(dat, grepl("Valid|Below", Comments))
   dat <- filter(dat, ViralLoad<2e7 | is.na(ViralLoad))
 
-  # Now replace undetectable with 0--1550
+  # Now replace undetectable with 1--1550
   Random = round(runif(nrow(dat), 1, 1549))
   cvl <- mutate(dat, ViralLoad = ifelse(is.na(ViralLoad), Random, ViralLoad),
-      log10VL = log10(ViralLoad),
       VLDetect = ifelse(ViralLoad>=1550 & !is.na(ViralLoad), 1, 0),
-      VLSuppress = ifelse(ViralLoad<1550 & !is.na(ViralLoad), 1, 0),
       AgeCat = cut(AgeAtVisit, breaks=Args$AgeCat, 
         include.lowest=TRUE, labels=NULL, right=FALSE))
   cvl <- select(cvl, IIntID, matches("Year"), Female, Age=AgeAtVisit, AgeCat,
-    DBSTestDate=SpecimenDate, ViralLoad, log10VL, VLDetect, VLSuppress)
-  cvl$HIVResult <- 1
+    DBSTestDate=SpecimenDate, ViralLoad, VLDetect)
   cvl
 }
