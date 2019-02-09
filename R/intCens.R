1db5df51b5b3c0ba676d0cf59d69bd65d63487b3
Alain <vando026@umn.edu>
2019-02-09 15:35:12 +0200

    Add intcens functions

--- R/intCens.R
+++ R/intCens.R
@@ -1,3 +1,50 @@
+#' @title getIntCensDates
+#' 
+#' @description  Make HIV test dates for IntCens model
+#' 
+#' @param Args requires Args, see \code{\link{setArgs}}.
+#' 
+#' @return data.frame
+#'
+#' @export 
+
+getIntCensDates <- function(Args) {
+  #
+  yr = getYearDates(Args$Years)
+  getDates <- function(i) {
+    yrs <- yr[yr>i$obs_start & yr <i$obs_end] 
+    s1 <- unique(c(yrs, i$obs_start, i$obs_end))
+    cbind(IIntID=i$IIntID, VisitDate=s1)
+  }
+  # GetDates
+  dat <- getHIV(Args)
+  early_neg <- getDatesMin(dat, "HIVNegative", "early_neg")
+  late_neg <- getDatesMax(dat, "HIVNegative", "late_neg")
+  early_pos <- getDatesMin(dat, "HIVPositive", "early_pos")
+  dat <- distinct(dat, IIntID, Female)
+  pdat <- Reduce(left_join, 
+    list(dat, early_neg, late_neg, early_pos))
+  pdat <- group_by(pdat, IIntID) %>% summarize(
+    obs_start = min(early_neg, late_neg, early_pos, na.rm=TRUE),
+    obs_end = max(early_neg, late_neg, early_pos, na.rm=TRUE)) 
+  # Split by ID
+  pdat <- split(pdat, pdat$IIntID)
+  tdat <- as_tibble(do.call(rbind, lapply(pdat, getDates)))
+  tdat <- Reduce(left_join, list(tdat, early_pos, dat))
+  tdat <- arrange(tdat, IIntID, VisitDate)
+  tdat <- mutate(tdat, 
+    event = as.numeric(!is.na(early_pos) & VisitDate==early_pos),
+    VisitDate = as.Date(VisitDate),
+    Year = format(VisitDate, "%Y"))
+  tdat <- select(tdat, -(early_pos))
+  tdat <- setData(tdat, time2="VisitDate")
+  tdat <- data.frame(tdat)
+  Origin <- as.Date(paste0(Args$Year[1], "-01-01"), origin="1970-01-01")
+  tdat$Time <- as.numeric(difftime(tdat$VisitDate, Origin), units="days")
+  tdat
+}
+
+
 #' @title IntCensParse
 #' 
 #' @description  Code to get IntCens results from output.
@@ -10,40 +57,37 @@ IntCensParse <- function(File=NULL) {
 
   out <- readLines(File)
   out <- gsub("\\t", " ", out)
+  out <- out[out!=""]
 
   # get lines of surv data
-  time_ln <- grep("^\\<Time\\>[[:space:]]+Estimate", out)
+  time_ln <- grep("^Time[[:space:]]+Estimate", out)
   surv_dat <- out[(time_ln+1):length(out)] 
-  surv_dat <- strsplit(surv_dat, " ")
-  surv_dat <- do.call(rbind, surv_dat)
-  surv_dat <- data.frame(apply(surv_dat, 2, as.numeric))
+  surv_dat <- strsplit(surv_dat, "[[:space:]]+")
+  surv_dat <- data.frame(do.call(rbind, surv_dat), stringsAsFactors=FALSE)
+  surv_dat[] <- lapply(surv_dat[],  as.numeric)
   colnames(surv_dat) <-  c("Time", "Estimate")
 
   # Now get estimates into R object
-  Cov_ln <- grep("^\\<Covariate\\>", out)
-  Cov_ln2 <- grep("[Co]?[vV]ariance", out)-1
-  emat <- out[(Cov_ln+1):Cov_ln2] 
-  emat <- strsplit(emat, " ")
+  Cov_ln <- grep("^Covariate", out)
+  Cov_ln2 <- grep("[Co]?[vV]ariance", out)
+  emat <- out[(Cov_ln+1):(Cov_ln2-1)] 
+  emat <- strsplit(emat, "[[:space:]]+")
   emat <- data.frame(do.call(rbind, emat), stringsAsFactors=FALSE)
-  emat[, -1] <- apply(emat[, -1], 2, as.numeric)
-  colnames(emat) <-  unlist(strsplit(out[Cov_ln], " "))
+  emat[, -1] <- lapply(emat[, -1], as.numeric)
+  colnames(emat) <-  strsplit(out[Cov_ln], "[[:space:]]+")[[1]]
 
-  # # Now covariance estimates into R object
-  line1 <- unlist(strsplit(grep("[Co]?[vV]ariance", out, value=TRUE), ' '))
-  lset <- ifelse(length(line1)==1, 1, 2) 
-  cov_ln <- grep("[Co]?[vV]ariance", out) + lset
-  cov_ln2 <- grep("Cummulative Hazard", out)-1
-  cmat <- out[cov_ln:cov_ln2] 
-  cmat <- strsplit(cmat, " ")
+  # Covariance estimates
+  ofset <- ifelse(grepl("^Var", out[Cov_ln2]), 1, 2) # needed
+  cmat <- out[(Cov_ln2+ofset):(time_ln-2)] 
+  cmat <- gsub("^\\s+|:", "", cmat)
+  cmat <- strsplit(cmat, "[[:space:]]+")
   cmat <- data.frame(do.call(rbind, cmat), stringsAsFactors=FALSE)
-  nm <- gsub(":", "", cmat[, 2])
-  cmat <- cmat[, -c(1,2)]
-  cmat <- apply(cmat, 2, as.numeric)
+  cmat[,-1] <- lapply(cmat[,-1], as.numeric)
+  nm <- cmat[,1]; cmat <- cmat[,-1, drop=FALSE]
   colnames(cmat) <-  rownames(cmat) <- nm
 
   list(sdat=surv_dat, edat=emat, cdat=cmat)
 }
-
 
 #' @title UniReg
 #' 
