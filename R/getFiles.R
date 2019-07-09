d0b3f8fa3cc641c109f6259090b653c475097f61
Alain <vando026@umn.edu>
2019-07-09 17:46:36 +0200

    Add ARTCLU

--- R/getFiles.R
+++ R/getFiles.R
@@ -30,7 +30,7 @@ getFiles <- function(
   pipfile="Derived/BoundedStructure/PIPBoundedStructures.xlsx",
   bscfile="Derived/BoundedStructure/BSIntID_Coords.csv",
   fem_art="Source/ARTemis/2017/ART_Females_Jun21.csv",
-  mal_art="Source/ARTemis/2017/ART_Males_Jun21.csv",
+  mal_art="Source/ARTemis/2017/ART_Clusters.csv",
   pvlfile="Source/CVL_2011_2014/Community Viral Load 2011-2014.dta") {
   flist <- as.list(environment())
   flist <- lapply(flist, function(x) file.path(root, x))
