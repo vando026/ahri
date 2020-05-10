21019e1de43794c17473cd98f59dbcf20f515def
Alain <vando026@umn.edu>
2020-05-10 23:06:22 +0200

    add docs

--- R/ahri.R
+++ R/ahri.R
@@ -1,10 +1,15 @@
 #' ahri: R package for the AHRI datasets
 #'
-#' @section The ahri package provides functions for analyzing the AHRI datasets. 
+#' The ahri package provides functions for analyzing the AHRI datasets. 
 #'
 #' @section Default .dta datasets:
 #' The ahri package uses the following deafult datasets, which you must request from the AHRI data
 #' centre.
+#'  - \code{RD05-99 ACDIS HIV All.dta}
+#'  - \code{SurveillanceEpisodesExtended.dta}
+#'  - \code{RD03-99 ACDIS WGH ALL.dta}
+#'  - \code{RD04-99 ACDIS MGH ALL.dta}
+#'  - \code{RD01-03 ACDIS BoundedStructures.dta}
 #' 
 #' @section setFile function:
 #' You can set the filepaths to the default .dta datasets using the \code{\link{setFiles}} function.  
