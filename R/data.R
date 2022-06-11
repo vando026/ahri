03a5f1caea4f884698b4458ec846fb49df80e260
Alain <albundy79@gmail.com>
2022-06-11 14:20:37 -0700

    Change names of ex data

--- R/data.R
+++ R/data.R
@@ -1,5 +1,22 @@
 #' Example datasets for G-imputation
 #'
+#' The repeat-testers dataset, with one row per individual, consisting of the start date,
+#' latest-negative and earliest-positive date. Your dataset must have these names to work
+#' with the \code{ahri} functions. 
+#'
+#' @format A data frame with 1022 rows and 6 variables:
+#' \describe{
+#' \item{IIntID}{The ID variable, which must have the name \code{IIntID}}
+#' \item{Female}{If participant female, 1=yes, 0=no}
+#' \item{obs_start}{The earliest-negative date}
+#' \item{late_neg}{The latest-negative date}
+#' \item{early_pos}{The earliest-positive date}
+#' \item{sero_event}{Indicates if repeat-tester seroconverted during observation period}
+#' }
+"rtdat_demo"
+
+#' Example datasets for G-imputation
+#'
 #' A dataset consisting of the covariate values that will be input for the unireg
 #' function. 
 #'
@@ -21,19 +38,3 @@
 
 
 
-#' Example datasets for G-imputation
-#'
-#' The repeat-testers dataset, with one row per individual, consisting of the start date,
-#' latest-negative and earliest-positive date. Your dataset must have these names to work
-#' with the \code{ahri} functions. 
-#'
-#' @format A data frame with 1022 rows and 6 variables:
-#' \describe{
-#' \item{IIntID}{The ID variable, which must have the name \code{IIntID}}
-#' \item{Female}{If participant female, 1=yes, 0=no}
-#' \item{obs_start}{The earliest-negative date}
-#' \item{late_neg}{The latest-negative date}
-#' \item{early_pos}{The earliest-positive date}
-#' \item{sero_event}{Indicates if repeat-tester seroconverted during observation period}
-#' }
-"rtdat_demo"
