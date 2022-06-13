a8940fb1a1af8a34a2065311966cc4bdd311fe84
Alain <albundy79@gmail.com>
2022-06-13 10:09:16 -0700

    Add quick start HIV calcs

--- R/ahri.R
+++ R/ahri.R
@@ -1,9 +1,9 @@
 #' ahri: R package for estimating HIV incidence with the AHRI datasets
 #'
-#' The ahri package provides functions for estimating the HIV incidence with
-#' AHRI datasets.
+#' The ahri package provides functions for estimating the annual HIV incidence
+#' rates with AHRI datasets.
 #'
-#' @section Default .dta datasets:
+#' @section AHRI datasets:
 #' The ahri package uses the following default datasets as of 2020, which you
 #' must request from the AHRI data centre.
 #'   [\code{RD05-99 ACDIS HIV All.dta}]
@@ -11,9 +11,12 @@
 #'   [\code{RD03-99 ACDIS WGH ALL.dta}]
 #'   [\code{RD04-99 ACDIS MGH ALL.dta}]
 #'   [\code{RD01-03 ACDIS BoundedStructures.dta}]
-#' @section setFile function:
-#' You can set the filepaths to the default .dta datasets
-#' using the \code{\link{setFiles}} function.
+#' You have to request these datasets from the AHRI data centre. 
+#' 
+#' @section Getting Started:
+#' Please see the https://github.com/vando026/ahri/wiki/1-Getting-started for a
+#' detailed vignette on how to get started with this package.
+#' 
 #' @seealso https://github.com/vando026/ahri
 #' @docType package
 #' @name ahri
